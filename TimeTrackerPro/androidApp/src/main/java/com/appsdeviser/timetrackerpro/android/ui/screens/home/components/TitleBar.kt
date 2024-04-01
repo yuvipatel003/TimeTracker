01b029e5cb2461c4fd764d41792f43d3ccec32c2
Yuvraj Patel <yuvipatel003@gmail.com>
2024-04-01 13:49:12 -0400

    Updated kotlin version. - Added Home Screen and Title bar. - Directly redirected to Home Screen rather splash. To avoid api call.

--- TimeTrackerPro/androidApp/src/main/java/com/appsdeviser/timetrackerpro/android/ui/screens/home/components/TitleBar.kt
+++ TimeTrackerPro/androidApp/src/main/java/com/appsdeviser/timetrackerpro/android/ui/screens/home/components/TitleBar.kt
@@ -1,5 +1,6 @@
 package com.appsdeviser.timetrackerpro.android.ui.screens.home.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
@@ -16,6 +17,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.appsdeviser.timetrackerpro.android.ui.core.theme.LightBlue
 import com.appsdeviser.timetrackerpro.android.ui.core.theme.LocalSpacing
 
 @Composable
@@ -23,36 +26,42 @@ fun TitleBar(
     modifier: Modifier = Modifier,
     title: String = "",
     onSettingsClick: () -> Unit,
-    onNotificationClick: () -> Unit
+    onNotificationClick: () -> Unit,
+    isNotificationSelected: Boolean = false,
+    isNotificationFeatureEnabled: Boolean = false
 ) {
     val spacing = LocalSpacing.current
-    
     Box(
         modifier = modifier
-            .padding(spacing.spaceMedium, spacing.spaceSmall)
+            .background(MaterialTheme.colorScheme.primary)
+            .padding(spacing.spaceMedium, spacing.spaceSmall),
     ) {
         Row(
             modifier = Modifier.align(Alignment.CenterStart)
         ) {
             Text(
                 text = title,
-                style = MaterialTheme.typography.titleSmall
+                style = MaterialTheme.typography.titleSmall,
+                color = MaterialTheme.colorScheme.onPrimary
             )
         }
 
         Row(
             modifier = Modifier.align(Alignment.CenterEnd)
         ) {
-            Icon(
-                imageVector = Icons.Default.Notifications, 
-                contentDescription = "Notifications",
-                modifier = Modifier
-                    .size(spacing.titleBarIconSize)
-                    .clickable {
-                        onNotificationClick()
-                    }
-            )
-            Spacer(modifier = Modifier.width(spacing.spaceMedium))
+            if(isNotificationFeatureEnabled) {
+                Icon(
+                    imageVector = Icons.Default.Notifications,
+                    contentDescription = "Notifications",
+                    modifier = Modifier
+                        .size(spacing.titleBarIconSize)
+                        .clickable {
+                            onNotificationClick()
+                        },
+                    tint = if (isNotificationSelected) LightBlue else MaterialTheme.colorScheme.onPrimary
+                )
+                Spacer(modifier = Modifier.width(spacing.spaceMedium))
+            }
             Icon(
                 imageVector = Icons.Default.Settings,
                 contentDescription = "Settings",
@@ -60,7 +69,8 @@ fun TitleBar(
                     .size(spacing.titleBarIconSize)
                     .clickable {
                         onSettingsClick()
-                    }
+                    },
+                tint = MaterialTheme.colorScheme.onPrimary
             )
         }
     }
