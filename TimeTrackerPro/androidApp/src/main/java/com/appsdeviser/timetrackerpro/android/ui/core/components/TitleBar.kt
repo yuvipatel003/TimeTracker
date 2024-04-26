cc94181fa03c05f0b5918cb79d392a175bd3382a
Yuvraj Patel <yuvipatel003@gmail.com>
2024-04-25 22:20:45 -0400

    Added Empty Screens for following - Settings - Add Record - View All Record - Add Category - View All Category

---
+++ TimeTrackerPro/androidApp/src/main/java/com/appsdeviser/timetrackerpro/android/ui/core/components/TitleBar.kt
@@ -0,0 +1,46 @@
+package com.appsdeviser.timetrackerpro.android.ui.core.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIosNew
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.appsdeviser.timetrackerpro.android.R
+import com.appsdeviser.timetrackerpro.android.ui.core.theme.LocalSpacing
+
+@Composable
+fun TitleBar(
+    modifier: Modifier = Modifier,
+    title: String = "",
+    onBackClick: () -> Unit
+) {
+    val spacing = LocalSpacing.current
+    Box(
+        modifier = modifier
+            .background(MaterialTheme.colorScheme.primary)
+            .padding(spacing.spaceMedium, spacing.spaceSmallMedium)
+    ) {
+        Icon(
+            imageVector = Icons.Default.ArrowBackIosNew,
+            contentDescription = stringResource(id = R.string.navigation_back),
+            modifier = Modifier.align(Alignment.CenterStart)
+                .clickable {
+                    onBackClick()
+                }
+            )
+        Text(
+            text = title,
+            style = MaterialTheme.typography.titleMedium,
+            modifier = Modifier.align(Alignment.Center)
+        )
+    }
+}
