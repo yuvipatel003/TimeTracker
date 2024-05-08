1b017b47cd09b42bb8f101db46dc529d58a1a1cb
Yuvraj Patel <yuvipatel003@gmail.com>
2024-05-08 16:33:17 -0400

    Code Reformating and Clean up. Removed unused classes.

--- TimeTrackerPro/shared/core/core-api/src/androidMain/kotlin/com/appsdeviser/core_api/Platform.android.kt
+++
@@ -1,7 +0,0 @@
-package com.appsdeviser.core_api
-
-class AndroidPlatformCoreApi : PlatformCoreApi {
-    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
-}
-
-actual fun getPlatformCoreApi(): PlatformCoreApi = AndroidPlatformCoreApi()
--- TimeTrackerPro/shared/core/core-common/src/commonMain/kotlin/com/appsdeviser/core_common/PlatformCoreCommon.kt
+++
@@ -1,7 +0,0 @@
-package com.appsdeviser.core_common
-
-interface PlatformCoreCommon {
-    val name: String
-}
-
-expect fun getPlatformCoreCommon(): PlatformCoreCommon
